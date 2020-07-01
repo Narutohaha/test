5f80daf6e3804ca6e9e8286b5e0dd30f37a55db6
liyong <liyong@cadg.cn>
2020-07-01 17:22:34 +0800

    初始化

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using WpfApp1;
+using System.Runtime.InteropServices;
+
+namespace ClassLibrary1
+{
+
+    [Transaction(TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+
+        public delegate bool EnumWindowsProc(int hWnd, int lParam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumChildWindows(IntPtr hwndParent, EnumWindowsProc lpEnumFunc, IntPtr lParam);
+        [DllImport("user32.dll")]
+        public static extern int GetWindowText(int hWnd, IntPtr lpString, int nMaxCount);
+        List<string> list = new List<string>();
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //IntPtr intPtr = Autodesk.Windows.ComponentManager.ApplicationWindow;
+           // EnumChildWindows(intPtr, this.EnumWindowsMethod, IntPtr.Zero);
+
+           // TaskDialog.Show("dasda", list.Count.ToString());
+
+
+            MainWindow win = new MainWindow();
+            win.Show();
+
+            return Result.Succeeded;
+        }
+
+
+        private bool EnumWindowsMethod(int hWnd, int lParam)
+        {
+            IntPtr lpString = Marshal.AllocHGlobal(200);
+            GetWindowText(hWnd, lpString, 200);
+            var text = Marshal.PtrToStringAnsi(lpString);
+
+            list.Add(text);
+            return true;
+        }
+
+
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public delegate bool EnumWindowsProc(int hWnd, int lParam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumChildWindows(IntPtr hwndParent, EnumWindowsProc lpEnumFunc, IntPtr lParam);
+        [DllImport("user32.dll")]
+        public static extern int GetWindowText(int hWnd, IntPtr lpString, int nMaxCount);
+
+        [DllImport("USER32.DLL", EntryPoint = "SendMessage")]
+        private static extern int SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, string lParam);
+
+        List<IntPtr> list = new List<IntPtr>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            list.Clear();
+            IntPtr intPtr = Autodesk.Windows.ComponentManager.ApplicationWindow;
+            EnumChildWindows(intPtr, this.EnumWindowsMethod, IntPtr.Zero);
+            
+            MessageBox.Show(list.Count.ToString());
+        }
+        private bool EnumWindowsMethod(int hWnd, int lParam)
+        {
+            IntPtr lpString = Marshal.AllocHGlobal(200);
+            GetWindowText(hWnd, lpString, 200);
+            var text = Marshal.PtrToStringAnsi(lpString);
+            if (text.Contains("宽度"))
+            {
+                MessageBox.Show(text);
+            }
+            list.Add(new IntPtr(hWnd));
+            return true;
+        }
+    }
+}
